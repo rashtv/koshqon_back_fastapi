873d363c1e6f7353b7e678b8c4e0e72bf6603ad4
Ilyas Rashitov <rashtv.19@gmail.com>
2024-04-15 22:25:50 +0500

    upd:announcement_images

--- src/app/api/databases/announcement.py
+++ src/app/api/databases/announcement.py
@@ -2,7 +2,9 @@ from datetime import datetime
 from typing import List
 
 from api.databases.db_client import (
-    announcements_collection, announcement_types_collection,
+    announcements_collection,
+    announcement_types_collection,
+    announcements_images_collection,
 )
 
 
@@ -36,12 +38,20 @@ async def retrieve_announcement_type(announcement_type_id: int):
     return announcement_type_helper(announcement_type)
 
 
+async def retrieve_announcement_images(announcement_id: int):
+    announcement_images = await announcements_images_collection.find({'announcements_id': announcement_id})
+    if announcement_images:
+        return announcement_images
+
+
 async def retrieve_announcements() -> List:
     announcements = []
     async for announcement in announcements_collection.find():
         announcement_type = await retrieve_announcement_type(announcement.get('announcement_type'))
+        announcement_images = await retrieve_announcement_images(announcement.get('_id'))
         announcement = announcement_helper(announcement)
         announcement['announcement_type'] = announcement_type
+        announcement['images'] = announcement_images
         announcements.append(announcement)
     return announcements
 
@@ -50,6 +60,8 @@ async def retrieve_announcement(announcement_id: int) -> dict:
     announcement = await announcements_collection.find_one({"_id": announcement_id})
     if announcement:
         announcement_type = await retrieve_announcement_type(announcement.get('announcement_type'))
+        announcement_images = await retrieve_announcement_images(announcement.get('_id'))
         announcement = announcement_helper(announcement)
         announcement['announcement_type'] = announcement_type
+        announcement['images'] = announcement_images
         return announcement
